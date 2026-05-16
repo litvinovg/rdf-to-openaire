6513024832d4488c47d369788c02851a87186cb4
Georgy Litvinov <git@litvinovg.pro>
2026-05-16 12:59:53 -0300

    XSD Validation

--- src/test/java/org/vivoweb/rdf2openaire/ConversionTest.java
+++ src/test/java/org/vivoweb/rdf2openaire/ConversionTest.java
@@ -7,18 +7,23 @@ import java.io.File;
 import java.nio.file.Files;
 import java.util.stream.Stream;
 
+import javax.xml.XMLConstants;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Named;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.xml.sax.SAXException;
 import org.xmlunit.builder.Input;
 import org.xmlunit.matchers.CompareMatcher;
 import org.apache.commons.io.FileUtils;
@@ -28,6 +33,7 @@ class ConversionTest {
 	private static final String OPENAIRE_TEST = "openaire_test_xml";
 	private static final String RDF_TEST = "rdf_test_xml";
 	private static File rdf = new File("src/test/resources/rdf_test_xml");
+	private static File xsd = new File("src/test/resources/openaire-cerif-profile.xsd");
 	private static File xslt = new File("src/main/resources/rdf_to_openaire.xsl");
 	private static Transformer transformer;
 	private static boolean UPDATE = true;
@@ -52,6 +58,15 @@ class ConversionTest {
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
 		transformer.transform(inputSource, new StreamResult(output));
 		Source test = Input.fromByteArray(output.toByteArray()).build();
+		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		try {
+			Schema schema = schemaFactory.newSchema(xsd);
+			Validator validator = schema.newValidator();
+			validator.validate(test);
+		} catch (SAXException e) {
+			System.out.println(control.getPath() + " is NOT valid:" + e);
+			throw e;
+		}
 		if (UPDATE) {
 			control.getParentFile().mkdirs();
 			Files.write(control.toPath(), output.toByteArray());
